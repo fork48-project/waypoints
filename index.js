b060fe7c2de2fc360da7f74b70bd330c8068070c
thatretrodev <thatretrodev@outlook.com>
2022-05-21 17:14:01 -0400

    Remove useless comments

--- index.js
+++ index.js
@@ -5,18 +5,9 @@ const app = express();
 const expressWs = require('express-ws')(app);
 const port = 3000;
 
-/*app.get('/go.js', (req, res) => {
-	res.sendFile(path.resolve(__dirname, "./go.js"));
-});*/
-
 app.ws('/ws', function(ws, req) {
 	ws.on('message', function(msg) {
 		var msgJSON = JSON.parse(msg);
-		
-		/*if (msgJSON.type == "hellofromgojs") {
-			console.log(`Hello from go.js! Mazean Scene ID: ${msgJSON.data.sceneID}`);
-		}
-		else {*/
 
 		if (!fs.existsSync("waypoints.json")) {
 			fs.writeFileSync("waypoints.json", "[]");
@@ -29,7 +20,6 @@ app.ws('/ws', function(ws, req) {
 		fs.writeFileSync("waypoints.json", JSON.stringify(waypointData));
 
 		console.log(msg);
-		//}
 
 		ws.send(msg);
 	});
