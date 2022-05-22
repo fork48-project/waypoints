9fddc103dcb328d9245b109bdd0b1dc5fbb0a485
thatretrodev <thatretrodev@outlook.com>
2022-05-22 12:12:16 -0400

    Update server

--- server/index.js
+++ server/index.js
@@ -1,27 +1,51 @@
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
+const mkdirp = require('mkdirp');
 const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
-const port = 3000;
+const port = 8080;
 
-app.ws('/ws', function(ws, req) {
-	ws.on('message', function(msg) {
-		var msgJSON = JSON.parse(msg);
+let connections = 0;
 
-		if (!fs.existsSync("waypoints.json")) {
-			fs.writeFileSync("waypoints.json", "[]");
+app.ws("/ws", function(ws, req) {
+	if (connections > 50) {
+		ws.close();
+		return;
+	}
+
+	connections++;
+
+	const sessionID = crypto.randomBytes(16).toString("hex");
+
+	ws.on("message", function(msg) {
+		// Parse message
+
+		try {
+			JSON.parse(msg);
+		} catch (e) {
+			ws.close();
+			return;
+		}
+
+		if (!fs.existsSync(`waypoints/${sessionID}.json`)) {
+			mkdirp.sync("waypoints");
+
+			fs.writeFileSync(`waypoints/${sessionID}.json`, "[]");
 		}
 		
-		const waypointData = JSON.parse(fs.readFileSync("waypoints.json"));
+		let waypointData = JSON.parse(fs.readFileSync(`waypoints/${sessionID}.json`));
 
-		waypointData.push(msgJSON);
+		waypointData.push(JSON.parse(msg));
 
-		fs.writeFileSync("waypoints.json", JSON.stringify(waypointData));
-
-		console.log(msg);
+		fs.writeFileSync(`waypoints/${sessionID}.json`, JSON.stringify(waypointData));
 
 		ws.send(msg);
+	});
+
+	ws.on("close", () => {
+		connections--;
 	});
 });
 
